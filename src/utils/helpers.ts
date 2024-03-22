9b3752bb78060ad1ef23dd832f922120c5490152
zana-abdollahpour <zana.ap4@gmail.com>
2024-03-22 21:19:27 +0330

    add date, time and currency helpers

---
+++ src/utils/helpers.ts
@@ -0,0 +1,24 @@
+// formats an amount of currency in the desired style
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "EUR",
+  }).format(value);
+}
+
+// prettifies the date provided
+export function formatDate(dateStr: string) {
+  return new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(dateStr));
+}
+
+// calculates the time difference between two times in minutes
+export function calcMinutesLeft(dateStr: string) {
+  const d1 = new Date().getTime();
+  const d2 = new Date(dateStr).getTime();
+  return Math.round((d2 - d1) / 60000);
+}
