14768bd4e9ad2918560b2c72eec08b3c0ca58131
zana-abdollahpour <zana.ap4@gmail.com>
2024-03-22 21:39:08 +0330

    update tailwind configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <main>Initial Commit</main>;
+  return (
+    <div>
+      <h1>
+        The best pizza.
+        <br />
+        Straight out of the oven, straight to you.
+      </h1>
+    </div>
+  );
 }
