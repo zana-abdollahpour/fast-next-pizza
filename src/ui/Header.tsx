1a19313b43bb17026c3b0117996b056e42d3c55c
zana-abdollahpour <zana.ap4@gmail.com>
2024-03-26 14:56:41 +0330

    update title and favicon

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link href="/" className="tracking-widest">
-        Fast React Pizza Co.
+        Fast Next Pizza Co.
       </Link>
 
       <SearchOrder />
