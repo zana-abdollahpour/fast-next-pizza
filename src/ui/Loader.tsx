5e7333b2d65ecab629b85286228317eab9f973ad
zana-abdollahpour <zana.ap4@gmail.com>
2024-03-28 12:29:53 +0330

    add mini loader variation

--- src/ui/Loader.tsx
+++ src/ui/Loader.tsx
@@ -1,4 +1,11 @@
-export default function Loader() {
+export default function Loader({ isMini = false }: { isMini: boolean }) {
+  if (isMini)
+    return (
+      <div className="fixed left-1/2 top-1/4 flex h-52 w-52 -translate-x-1/2 items-center justify-center p-4">
+        <div className="spinner"></div>
+      </div>
+    );
+
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
       <div className="loader"></div>
