821f20d33559f66ca7a324aed00d349a3aec2c20
zana-abdollahpour <zana.ap4@gmail.com>
2024-03-30 10:43:25 +0330

    improve Modal design

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -6,8 +6,7 @@ interface ButtonProps {
   children: React.ReactNode;
   disabled?: boolean;
   href?: string;
-  color?: "yellow" | "red";
-  type: "primary" | "small" | "secondary" | "round" | "submit";
+  type: "primary" | "small" | "secondary" | "round" | "submit" | "cancel";
   onClick?: (() => void) | ((e: FormEvent<HTMLButtonElement>) => void);
 }
 
@@ -17,12 +16,15 @@ export default function Button({
   href = "",
   type,
   onClick,
-  color = "yellow",
 }: ButtonProps) {
   const { pending } = useFormStatus();
 
-  const baseStyles = `inline-block text-sm rounded-full font-semibold uppercase tracking-wide transition-colors duration-300  focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed`;
-  const mainStyles = `bg-${color}-400 text-stone-800 hover:bg-${color}-300 focus:bg-${color}-300 focus:ring-${color}-300`;
+  const baseStyles =
+    "inline-block text-sm rounded-full font-semibold uppercase tracking-wide transition-colors duration-300  focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed";
+  const mainStyles =
+    "bg-yellow-400 text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-yellow-300";
+  const cancelStyles =
+    "bg-red-500 text-stone-800 hover:bg-red-400 focus:bg-red-400 focus:ring-red-400";
   const secondaryStyles =
     "border-2 border-stone-300 text-stone-600 hover:text-stone-800 focus:text-stone-800 focus:ring-stone-200 px-4 py-2.5 md:px-6 md:py-3.5";
 
@@ -32,6 +34,7 @@ export default function Button({
     small: `${baseStyles} ${mainStyles} px-4 py-2 md:px-5 md:py-2.5 text-xs`,
     round: `${baseStyles} ${mainStyles} px-2.5 py-1 md:px-3.5 md:py-2 text-sm`,
     submit: `${baseStyles} ${mainStyles} px-4 py-3 md:px-6 md:py-4 disabled:bg-slate-300`,
+    cancel: `${baseStyles} ${cancelStyles} px-4 py-3 md:px-6 md:py-4 disabled:bg-slate-300`,
   };
 
   if (href)
--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -19,23 +19,19 @@ export default function Modal({ children }: ModalProps) {
   return isOpen
     ? createPortal(
         <div className="fixed left-0 top-0 z-50 h-screen w-screen bg-yellow-400/10 backdrop-blur-sm">
-          <div className="absolute left-1/2 top-1/2 flex h-96 w-80 -translate-x-1/2 -translate-y-1/2 flex-col justify-between rounded-xl bg-yellow-700 p-8">
+          <div className="absolute left-1/2 top-1/2 flex h-96 w-80 -translate-x-1/2 -translate-y-1/2 flex-col justify-between rounded-xl bg-yellow-600/95 p-8">
             <p className="text-3xl text-yellow-50">{children}</p>
             <div className="flex justify-between">
               <Button
+                type="cancel"
                 onClick={() => {
                   dispatch(clearCart());
                   setIsOpen(false);
                 }}
-                type="submit"
               >
                 Clear
               </Button>
-              <Button
-                type="primary"
-                color="red"
-                onClick={() => setIsOpen(false)}
-              >
+              <Button type="primary" onClick={() => setIsOpen(false)}>
                 Keep Items
               </Button>
             </div>
