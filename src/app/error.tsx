6c8b178613493963f813c8fd01edbe4af9e1866e
zana-abdollahpour <zana.ap4@gmail.com>
2024-03-30 10:00:43 +0330

    improve error page UX / UI

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -10,12 +10,17 @@ interface ErrorPageProps {
 export default function Error({ error, reset }: ErrorPageProps) {
   return (
     <main className="mt-8 flex h-full flex-col items-center justify-center gap-8 text-xl">
-      <h2 className="text-center">Something went wrong!</h2>
-      <h2 className="text-center">{error.message}</h2>
+      <h2 className="text-center text-stone-400">Something went wrong!</h2>
+      <h2 className="text-center">Can&apos;t find #{error.digest} order!</h2>
       {/* // Attempt to recover by trying to re-render the invoices route */}
-      <Button onClick={() => reset()} type="primary">
-        Try again
-      </Button>
+      <div className="flex items-center justify-center gap-10">
+        <Button type="primary" href="/">
+          &larr; Back to Home
+        </Button>
+        <Button onClick={() => reset()} type="secondary">
+          Try again
+        </Button>
+      </div>
     </main>
   );
 }
